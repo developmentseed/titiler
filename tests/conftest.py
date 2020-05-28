8b98f6f6fecc4ba9d12ab795914c6b1e7cc9ffdb
Vincent Sarago <vincent.sarago@gmail.com>
2020-05-28 11:00:55 -0400

    add custom colormaps (#24)

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,12 @@ from rasterio.io import DatasetReader
 def app(monkeypatch) -> TestClient:
     """Make sure we use monkeypatch env."""
     monkeypatch.setenv("DISABLE_CACHE", "YESPLEASE")
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "jqt")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "rde")
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-2")
+    monkeypatch.setenv("AWS_REGION", "us-west-2")
+    monkeypatch.delenv("AWS_PROFILE", raising=False)
+    monkeypatch.setenv("AWS_CONFIG_FILE", "/tmp/noconfigheere")
 
     from titiler.main import app
 
@@ -23,4 +29,5 @@ def mock_rio(src_path: str) -> DatasetReader:
     """Mock rasterio.open."""
     prefix = os.path.join(os.path.dirname(__file__), "fixtures")
     assert src_path.startswith("https://myurl.com/")
-    return rasterio.open(os.path.join(prefix, "cog.tif"))
+    cog_path = os.path.basename(src_path)
+    return rasterio.open(os.path.join(prefix, cog_path))
