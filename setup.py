f4fdc02ea0235470589eeb34a4da8e5aae74e696
vincentsarago <vincent.sarago@gmail.com>
2020-06-04 10:40:51 -0400

    add changelog

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ extra_reqs = {
 
 setup(
     name="titiler",
-    version="0.1.0",
+    version="1.0.0",
     description=u"",
     long_description=long_description,
     long_description_content_type="text/markdown",
