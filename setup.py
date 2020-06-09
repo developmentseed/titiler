05a587de18080919d200e9f4b19ddc250f31fb6e
vincentsarago <vincent.sarago@gmail.com>
2020-06-09 11:35:35 -0400

    update changelog [skip ci]

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ extra_reqs = {
 
 setup(
     name="titiler",
-    version="1.1.0",
+    version="2.0.0",
     description=u"",
     long_description=long_description,
     long_description_content_type="text/markdown",
