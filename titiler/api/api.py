ee9903c3bde431781a3232d441d684cd11e2c355
Jeff Albrecht <geospatialjeff@gmail.com>
2020-06-26 16:49:21 -0500

    include mosaicjson router

--- titiler/api/api.py
+++ titiler/api/api.py
@@ -1,23 +1,34 @@
 """titiler api."""
+import importlib
 
 from titiler.api.endpoints import cog, tms
 
 from fastapi import APIRouter
 
-try:
-    from titiler.api.endpoints import stac
 
-    has_stac = True
-except ModuleNotFoundError:
-    has_stac = False
+def _include_extra_router(router: APIRouter, module: str, **kwargs) -> None:
+    """Helper function to add routers available through pip extras"""
+    try:
+        mod = importlib.import_module(module)
+        router.include_router(mod.router, **kwargs)  # type: ignore
+    except ModuleNotFoundError:
+        pass
 
 
 api_router = APIRouter()
 api_router.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
-
-if has_stac:
-    api_router.include_router(
-        stac.router, prefix="/stac", tags=["SpatioTemporal Asset Catalog"]
-    )
-
 api_router.include_router(tms.router)
+
+# Additional routers installed as pip extras
+_include_extra_router(
+    api_router,
+    module="titiler.api.endpoints.stac",
+    prefix="/stac",
+    tags=["SpatioTemporal Asset Catalog"],
+)
+_include_extra_router(
+    api_router,
+    module="titiler.api.endpoints.mosaic",
+    prefix="/mosaic",
+    tags=["MosaicJSON"],
+)
