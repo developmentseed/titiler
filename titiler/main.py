b855e6a19b40bdd8af8f0f22eddbdffbf22a824a
vincentsarago <vincent.sarago@gmail.com>
2020-07-31 09:00:17 -0400

    do not set cache control on 5** errors

--- titiler/main.py
+++ titiler/main.py
@@ -66,6 +66,7 @@ async def header_middleware(request: Request, call_next):
         not response.headers.get("Cache-Control")
         and settings.DEFAULT_CACHECONTROL
         and request.method in ["HEAD", "GET"]
+        and response.status_code < 500
     ):
         response.headers["Cache-Control"] = settings.DEFAULT_CACHECONTROL
     return response
