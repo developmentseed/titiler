1aed4b68ed7d891edf08c037216c3ff8a3aa687d
vincentsarago <vincent.sarago@gmail.com>
2020-10-13 17:44:54 -0400

    Bump version: 0.1.0a7 → 0.1.0a8

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extra_reqs = {
 
 setup(
     name="titiler",
-    version="0.1.0a7",
+    version="0.1.0a8",
     description=u"",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- titiler/__init__.py
+++ titiler/__init__.py
@@ -1,3 +1,3 @@
 """titiler."""
 
-__version__ = "0.1.0a7"
+__version__ = "0.1.0a8"
