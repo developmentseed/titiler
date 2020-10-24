898bcaf47817a5b18715fecb8189f80ea6ce36c5
Vincent Sarago <vincent.sarago@gmail.com>
2020-10-23 21:56:24 -0400

    quiet logs and add loggerMiddleware (#139)

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,7 +1,12 @@
 """AWS Lambda handler."""
 
+import logging
+
 from mangum import Mangum
 
 from titiler.main import app
+
+logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
+logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
 handler = Mangum(app, lifespan="auto", log_level="error")
