1b3d7b020cb513729d2b8e79cc2ccb41742b03ed
Denis Rykov <rykovd@gmail.com>
2021-02-02 20:38:22 +0100

    Move 'validate' in MosaicTilerFactory class (#206)

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -22,8 +22,8 @@ def test_MosaicTilerFactory(set_env):
     from titiler.endpoints import factory
 
     app = factory.MosaicTilerFactory()
-    assert len(app.router.routes) == 19
+    assert len(app.router.routes) == 20
     assert app.tms_dependency == WebMercatorTMSParams
 
     app = factory.MosaicTilerFactory(add_create=False, add_update=False)
-    assert len(app.router.routes) == 17
+    assert len(app.router.routes) == 18
