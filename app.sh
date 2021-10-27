3f138a8fe27f829275668cfebd32703983c63896
wxdavef <davef@saildrone.com>
2021-10-27 16:57:38 -0700

    app file to start uvicorn

---
+++ app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# limit server to max requests. It will be restarted by kubernetes
+# add some random jitter so every server doesn't restart at once
+max_requests=$((10000 + RANDOM/10))  # RANDOM is in range (0,32k)
+echo "starting uvicorn with max-requests $max_requests"
+
+exec uvicorn \
+    --workers 1 \
+    --limit-max-requests $max_requests \
+    --host 0.0.0.0 \
+    --port 8000 \
+    titiler.application.main:app
