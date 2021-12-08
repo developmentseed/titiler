a4b4ed44f761ae49de2709cdf4d0e44b8f018196
wxdavef <davef@saildrone.com>
2021-12-08 15:27:30 -0800

    using saildrone docker file, adding mosic backend

--- saildrone_custom_tiler/app/main.py
+++ saildrone_custom_tiler/app/main.py
@@ -4,12 +4,37 @@ app/main.py
 
 """
 
+import os
+import re
+
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+
+from titiler.core.dependencies import DefaultDependency
+from titiler.mosaic.factory import MosaicTilerFactory
 
 from .cache import setup_cache
 from .routes import cog
+
+MOSAIC_BACKEND = os.getenv("TITILER_MOSAIC_BACKEND")
+MOSAIC_HOST = os.getenv("TITILER_MOSAIC_HOST")
+
+
+def MosaicPathParams(
+    mosaic: str = Query(..., description="mosaic name")
+) -> str:
+    """Create dataset path from args"""
+    # mosaic name should be in form of `{user}.{layername}`
+    if not re.match(self.mosaic, r"^[a-zA-Z0-9-_]{1,32}\.[a-zA-Z0-9-_]{1,32}$"):
+        raise HTTPException(
+            status_code=400,
+                detail=f"Invalid mosaic name {self.input}.",
+            )
+
+        return f"{MOSAIC_BACKEND}{MOSAIC_HOST}/{self.input}.json.gz"
+
+
 
 app = FastAPI(title="My simple app with cache")
 
