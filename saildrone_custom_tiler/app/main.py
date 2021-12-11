68f41cb7d124951ba4e9a84a598f6004df0863d6
wxdavef <davef@saildrone.com>
2021-12-10 16:24:28 -0800

    needed healthz endpoint now that tiler is self built

--- saildrone_custom_tiler/app/main.py
+++ saildrone_custom_tiler/app/main.py
@@ -7,12 +7,17 @@ app/main.py
 import os
 import re
 
+from titiler.application.custom import templates
+
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
 from fastapi import FastAPI, HTTPException, Query
 
 from titiler.core.dependencies import DefaultDependency
 from titiler.mosaic.factory import MosaicTilerFactory
+
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from .cache import setup_cache
 from .routes import sd_cog
@@ -29,3 +34,21 @@ add_exception_handlers(app, DEFAULT_STATUS_CODES)
 
 #print(mosaic.router)
 app.include_router(sd_mosaic.router, prefix="/mosaic", tags=["Custom backend mosaic"])
+
+
+
+
+@app.get("/healthz", description="Health Check", tags=["Health Check"])
+def ping():
+    """Health check."""
+    return {"ping": "pong!"}
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def landing(request: Request):
+    """TiTiler Landing page"""
+    return templates.TemplateResponse(
+        name="index.html",
+        context={"request": request},
+        media_type="text/html",
+    )
