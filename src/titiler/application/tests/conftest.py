077649089dc51e5b9598b3150119e4858172f29b
vincentsarago <vincent.sarago@gmail.com>
2022-01-25 08:09:45 +0100

    fix tests for new rio-tiler

--- src/titiler/application/tests/conftest.py
+++ src/titiler/application/tests/conftest.py
@@ -43,6 +43,9 @@ def mock_RequestGet(src_path):
         def json(self):
             return json.loads(self.data)
 
+        def raise_for_status(self):
+            return True
+
     assert src_path.startswith("https://myurl.com/")
     stac_path = os.path.basename(src_path)
     with open(os.path.join(DATA_DIR, stac_path), "r") as f:
