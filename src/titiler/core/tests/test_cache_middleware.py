7b8d4d92660ca9b1c169211b8c4ffb08bc764812
Sean Harkins <sharkinsgis@gmail.com>
2022-03-03 12:58:05 -0600

    Make http error cache control behavior configurable. (#444)
    * Make http error cache control behavior configurable.
    
    * Update parameter naming as requested by @vincentsarago.

--- src/titiler/core/tests/test_cache_middleware.py
+++ src/titiler/core/tests/test_cache_middleware.py
@@ -5,6 +5,7 @@ from titiler.core.middleware import CacheControlMiddleware
 
 from fastapi import FastAPI, Path
 
+from starlette.responses import Response
 from starlette.testclient import TestClient
 
 
@@ -36,9 +37,19 @@ def test_cachecontrol_middleware_exclude():
         """tiles."""
         return "yeah"
 
+    @app.get("/emptytiles/{z}/{x}/{y}")
+    async def emptytiles(
+        z: int = Path(..., ge=0, le=30, description="Mercator tiles's zoom level"),
+        x: int = Path(..., description="Mercator tiles's column"),
+        y: int = Path(..., description="Mercator tiles's row"),
+    ):
+        """tiles."""
+        return Response(status_code=404)
+
     app.add_middleware(
         CacheControlMiddleware,
         cachecontrol="public",
+        cachecontrol_max_http_code=400,
         exclude_path={r"/route1", r"/route2", r"/tiles/[0-1]/.+"},
     )
 
@@ -58,3 +69,6 @@ def test_cachecontrol_middleware_exclude():
 
     response = client.get("/tiles/3/1/1")
     assert response.headers["Cache-Control"] == "public"
+
+    response = client.get("/emptytiles/3/1/1")
+    assert not response.headers.get("Cache-Control")
