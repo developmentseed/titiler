5d03d32af9e3b5f8f9f3b177c58d76a2ade8a3e8
vincentsarago <vincent.sarago@gmail.com>
2022-06-09 18:26:50 +0200

    0.6.0 -> 0.7.0

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 from . import dependencies, errors, factory, routing, utils  # noqa
 from .factory import (  # noqa
