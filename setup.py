650f6e45e3ad097360abf2e639b643cce83d3e45
vincentsarago <vincent.sarago@gmail.com>
2022-12-09 14:22:08 +0100

    Bump version: 0.9.0 → 0.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 inst_reqs = [
     f"titiler.core=={__version__}",
--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 from . import dependencies, errors, factory, routing  # noqa
 from .factory import (  # noqa
