e9a3002a08a4858f35d556641491ef3b574af5e0
vincentsarago <vincent.sarago@gmail.com>
2023-08-21 13:52:18 +0200

    Bump version: 0.13.0 → 0.13.1

--- src/titiler/extensions/titiler/extensions/__init__.py
+++ src/titiler/extensions/titiler/extensions/__init__.py
@@ -1,6 +1,6 @@
 """titiler.extensions"""
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 from .cogeo import cogValidateExtension  # noqa
 from .stac import stacExtension  # noqa
--- src/titiler/mosaic/titiler/mosaic/__init__.py
+++ src/titiler/mosaic/titiler/mosaic/__init__.py
@@ -1,6 +1,6 @@
 """titiler.mosaic"""
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 from . import errors, factory  # noqa
 from .factory import MosaicTilerFactory  # noqa
