5c65b89bc3372e6b8e4712cab024e00ec293b8f3
Enrico Ubaldi <enricoubaldi@gmail.com>
2023-08-21 13:47:29 +0200

    Corse allow methods in env options. #683 (#684)

--- src/titiler/application/titiler/application/settings.py
+++ src/titiler/application/titiler/application/settings.py
@@ -9,6 +9,7 @@ class ApiSettings(BaseSettings):
 
     name: str = "TiTiler"
     cors_origins: str = "*"
+    cors_allow_methods: str = "GET"
     cachecontrol: str = "public, max-age=3600"
     root_path: str = ""
     debug: bool = False
@@ -25,3 +26,8 @@ class ApiSettings(BaseSettings):
     def parse_cors_origin(cls, v):
         """Parse CORS origins."""
         return [origin.strip() for origin in v.split(",")]
+
+    @field_validator("cors_allow_methods")
+    def parse_cors_allow_methods(cls, v):
+        """Parse CORS allowed methods."""
+        return [method.strip().upper() for method in v.split(",")]
