07fa9fa5d539a1c278fd589dd52fd8ae1a734c14
Dan Schoppe <dan.schoppe@gmail.com>
2023-10-18 11:30:52 -0500

    Configure Mangum with root_path for API Gateway non-root routes

--- deployment/aws/lambda/handler.py
+++ deployment/aws/lambda/handler.py
@@ -5,8 +5,11 @@ import logging
 from mangum import Mangum
 
 from titiler.application.main import app
+from titiler.application.settings import ApiSettings
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
-handler = Mangum(app, lifespan="auto")
+api_settings = ApiSettings()
+
+handler = Mangum(app, api_gateway_base_path=api_settings.root_path, lifespan="auto")
