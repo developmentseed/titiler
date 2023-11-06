e5a01f6a5f9879556b3867c3f1df685ded195519
github-action-benchmark <github@users.noreply.github.com>
2023-11-06 11:57:35 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for c599c29b782657e69ad22caf59613276edf99539

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699271829111,
+  "lastUpdate": 1699271855010,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -2182,6 +2182,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad longest_transaction",
             "value": 0.08,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "distinct": true,
+          "id": "c599c29b782657e69ad22caf59613276edf99539",
+          "message": "Bump version: 0.15.3 → 0.15.4",
+          "timestamp": "2023-11-06T12:55:45+01:00",
+          "tree_id": "20d35cd764aa06aeeb3fe32947fab667b1ee96cc",
+          "url": "https://github.com/developmentseed/titiler/commit/c599c29b782657e69ad22caf59613276edf99539"
+        },
+        "date": 1699271854263,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.06,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 4.27,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.18,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.04,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.06,
             "unit": "s"
           }
         ]
