1b60bb5ff0a8b4dfde68f062694c8c619dbabda5
github-action-benchmark <github@users.noreply.github.com>
2024-01-17 08:52:23 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for 41fea96f029e3b18a5158a1ceb0a0fe85b611680

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704901085920,
+  "lastUpdate": 1705481543919,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -3229,6 +3229,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad elapsed_time",
             "value": 4.45,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.18,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.04,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.06,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "distinct": true,
+          "id": "41fea96f029e3b18a5158a1ceb0a0fe85b611680",
+          "message": "Bump version: 0.16.1 → 0.16.2",
+          "timestamp": "2024-01-17T09:50:52+01:00",
+          "tree_id": "8bdfe451b138c00144d9a3d5eb108ca5e5778995",
+          "url": "https://github.com/developmentseed/titiler/commit/41fea96f029e3b18a5158a1ceb0a0fe85b611680"
+        },
+        "date": 1705481543109,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.06,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 4.31,
             "unit": "s"
           },
           {
