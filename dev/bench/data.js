be1db858b3732b3f0983136398c61e4632d8d385
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 16:52:05 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for 393614149e47fd6e0711cb30cb67b24c4b9f5388

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712666258303,
+  "lastUpdate": 1712681525873,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -5176,6 +5176,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad elapsed_time",
             "value": 4.24,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.18,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.04,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.05,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "Vincent Sarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "393614149e47fd6e0711cb30cb67b24c4b9f5388",
+          "message": "add more tests and reduce code duplication (#806)\n\n* add more tests and reduce code duplication\r\n\r\n* fix item\r\n\r\n* fix item in titiler.application",
+          "timestamp": "2024-04-09T18:50:38+02:00",
+          "tree_id": "af5a9fccbbb36faa5c2bc4b961ce0d3811404845",
+          "url": "https://github.com/developmentseed/titiler/commit/393614149e47fd6e0711cb30cb67b24c4b9f5388"
+        },
+        "date": 1712681525280,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.05,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 4.08,
             "unit": "s"
           },
           {
