3ce3f63566ce5183b6e5909fdc302e09ecf723ac
vincentsarago <vincent.sarago@gmail.com>
2024-04-12 18:33:15 +0200

    Bump version: 0.18.0 → 0.18.1

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "0.18.0"
+__version__ = "0.18.1"
--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.18.0"
+__version__ = "0.18.1"
 
 from . import dependencies, errors, factory, routing  # noqa
 from .factory import (  # noqa
