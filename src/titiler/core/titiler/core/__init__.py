f7e7be3b24510cf6411baf035513807f34d5f457
vincentsarago <vincent.sarago@gmail.com>
2024-06-26 10:48:04 +0200

    Bump version: 0.18.3 → 0.18.4

--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.18.3"
+__version__ = "0.18.4"
 
 from . import dependencies, errors, factory, routing  # noqa
 from .factory import (  # noqa
--- src/titiler/mosaic/titiler/mosaic/__init__.py
+++ src/titiler/mosaic/titiler/mosaic/__init__.py
@@ -1,6 +1,6 @@
 """titiler.mosaic"""
 
-__version__ = "0.18.3"
+__version__ = "0.18.4"
 
 from . import errors, factory  # noqa
 from .factory import MosaicTilerFactory  # noqa
