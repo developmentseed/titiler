503e16b12fa608adcbf5374c108fcdaba8eb8e54
hrodmn <henry.rodman@gmail.com>
2024-09-20 11:31:51 -0500

    only show 'share' button when enable_viewer=True

--- src/titiler/extensions/titiler/extensions/viewer.py
+++ src/titiler/extensions/titiler/extensions/viewer.py
@@ -36,6 +36,8 @@ class cogViewerExtension(FactoryExtension):
                     ),
                     "info_endpoint": factory.url_for(request, "info"),
                     "statistics_endpoint": factory.url_for(request, "statistics"),
+                    "viewer_enabled": hasattr(factory, "add_viewer")
+                    and getattr(factory, "add_viewer", False),
                 },
                 media_type="text/html",
             )
