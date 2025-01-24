2a0267b1e9531a421a80884fc5f26d35980ed39b
Vincent Sarago <vincent.sarago@gmail.com>
2025-01-24 13:53:35 +0100

    0.20.1 -> 0.21.0 (#1078)

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "0.20.1"
+__version__ = "0.21.0"
--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.20.1"
+__version__ = "0.21.0"
 
 from . import dependencies, errors, factory, routing  # noqa
 from .factory import (  # noqa
