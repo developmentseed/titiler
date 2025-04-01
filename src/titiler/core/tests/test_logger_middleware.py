0c3689082d3f6f83d72825e9bf32ca0f3abbedcf
vincentsarago <vincent.sarago@gmail.com>
2025-04-01 10:48:02 +0200

    update tests

--- src/titiler/core/tests/test_logger_middleware.py
+++ src/titiler/core/tests/test_logger_middleware.py
@@ -2,6 +2,7 @@
 
 import json
 import logging
+from logging import config
 
 from fastapi import FastAPI
 from starlette.testclient import TestClient
@@ -20,61 +21,70 @@ def test_timing_middleware(caplog):
 
     app.add_middleware(LoggerMiddleware)
 
+    config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "request": {
+                    "format": (
+                        "%(asctime)s - %(levelname)s - %(name)s - %(message)s "
+                        + json.dumps(
+                            {
+                                k: f"%({k})s"
+                                for k in [
+                                    "method",
+                                    "referer",
+                                    "origin",
+                                    "path",
+                                    "path_params",
+                                    "query_params",
+                                    "headers",
+                                ]
+                            }
+                        )
+                    ),
+                },
+            },
+            "handlers": {
+                "console_request": {
+                    "class": "logging.StreamHandler",
+                    "level": "DEBUG",
+                    "formatter": "request",
+                    "stream": "ext://sys.stdout",
+                },
+            },
+            "loggers": {
+                "titiler-requests": {
+                    "level": "INFO",
+                    "handlers": ["console_request"],
+                    "propagate": True,
+                },
+            },
+        }
+    )
+
     with TestClient(app) as client:
-        caplog.clear()
-        client.get("/route1")
-        log = caplog.records[0]
-        assert log.name == "titiler"
-        assert log.levelname == "INFO"
-        req = json.loads(log.message.split("Request: ")[1])
-        assert req["path"] == "/route1"
+        with caplog.at_level(logging.INFO, logger="titiler-requests"):
+            caplog.clear()
+            client.get("/route1")
+            log = caplog.records[0]
+            assert log.name == "titiler-requests"
+            assert log.levelname == "INFO"
+            assert log.message == "Request received: /route1 GET"
+            assert hasattr(log, "query_params")
 
-        caplog.clear()
-        client.get("/route1", params={"hey": "yo"})
-        log = caplog.records[0]
-        req = json.loads(log.message.split("Request: ")[1])
-        assert req["path"] == "/route1"
-        assert req["query_params"] == {"hey": "yo"}
+            caplog.clear()
+            client.get("/route1", params={"hey": "yo"})
+            log = caplog.records[0]
+            assert log.message == "Request received: /route1 GET"
+            assert log.query_params == {"hey": "yo"}
 
-        caplog.clear()
-        client.get("/route1", params={"hey": "yo"}, headers={"accept-encoding": "gzip"})
-        log = caplog.records[0]
-        req = json.loads(log.message.split("Request: ")[1])
-        assert req["headers"]["accept-encoding"] == "gzip"
-
-
-def test_timing_middleware_option(caplog):
-    """Create App."""
-    app = FastAPI()
-
-    @app.get("/route1")
-    async def route1():
-        """route1."""
-        return "Yo"
-
-    logger = logging.getLogger("custom")
-    logger.setLevel(logging.INFO)
-
-    app.add_middleware(LoggerMiddleware, logger=logger, log_data=["path"])
-
-    with TestClient(app) as client:
-        caplog.clear()
-        client.get("/route1")
-        log = caplog.records[0]
-        assert log.name == "custom"
-        assert log.levelname == "INFO"
-        req = json.loads(log.message.split("Request: ")[1])
-        assert req["path"] == "/route1"
-
-        caplog.clear()
-        client.get("/route1", params={"hey": "yo"})
-        log = caplog.records[0]
-        req = json.loads(log.message.split("Request: ")[1])
-        assert req["path"] == "/route1"
-        assert "query_params" not in req
-
-        caplog.clear()
-        client.get("/route1", params={"hey": "yo"}, headers={"accept-encoding": "gzip"})
-        log = caplog.records[0]
-        req = json.loads(log.message.split("Request: ")[1])
-        assert "headers" not in req
+            caplog.clear()
+            client.get(
+                "/route1", params={"hey": "yo"}, headers={"accept-encoding": "gzip"}
+            )
+            log = caplog.records[0]
+            assert log.message == "Request received: /route1 GET"
+            assert log.query_params == {"hey": "yo"}
+            assert log.headers["accept-encoding"] == "gzip"
