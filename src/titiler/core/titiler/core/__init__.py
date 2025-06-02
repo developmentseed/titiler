03f9148135192468825795cf0557ebacb58db4ef
vincentsarago <vincent.sarago@gmail.com>
2025-06-02 22:35:58 +0200

    0.22.1 -> 0.22.2

--- src/titiler/core/titiler/core/__init__.py
+++ src/titiler/core/titiler/core/__init__.py
@@ -1,6 +1,6 @@
 """titiler.core"""
 
-__version__ = "0.22.1"
+__version__ = "0.22.2"
 
 from . import dependencies, errors, factory, routing  # noqa
 from .factory import (  # noqa
--- src/titiler/extensions/titiler/extensions/__init__.py
+++ src/titiler/extensions/titiler/extensions/__init__.py
@@ -1,6 +1,6 @@
 """titiler.extensions"""
 
-__version__ = "0.22.1"
+__version__ = "0.22.2"
 
 from .cogeo import cogValidateExtension  # noqa
 from .render import stacRenderExtension  # noqa
