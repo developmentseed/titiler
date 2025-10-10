1584af52a0a7b78fe6ca8e4fab571ef4db14c361
vincentsarago <vincent.sarago@gmail.com>
2025-10-10 15:40:26 +0200

    0.24.0 -> 0.24.1

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "0.24.0"
+__version__ = "0.24.1"
--- src/titiler/extensions/titiler/extensions/__init__.py
+++ src/titiler/extensions/titiler/extensions/__init__.py
@@ -1,6 +1,6 @@
 """titiler.extensions"""
 
-__version__ = "0.24.0"
+__version__ = "0.24.1"
 
 from .cogeo import cogValidateExtension  # noqa
 from .render import stacRenderExtension  # noqa
