ea8ad769dd416c67cb3334bd998b583eb0f202fb
github-action-benchmark <github@users.noreply.github.com>
2025-10-24 05:10:04 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for dd0e0d11147ffd0e10e089f8adb5de57c23075fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761282585468,
+  "lastUpdate": 1761282604855,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -15973,6 +15973,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad elapsed_time",
             "value": 4.08,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.18,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.04,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.05,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dd0e0d11147ffd0e10e089f8adb5de57c23075fa",
+          "message": "Bump actions/setup-node from 5 to 6 in the all group (#1243)\n\nBumps the all group with 1 update: [actions/setup-node](https://github.com/actions/setup-node).\n\n\nUpdates `actions/setup-node` from 5 to 6\n- [Release notes](https://github.com/actions/setup-node/releases)\n- [Commits](https://github.com/actions/setup-node/compare/v5...v6)\n\n---\nupdated-dependencies:\n- dependency-name: actions/setup-node\n  dependency-version: '6'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n  dependency-group: all\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-10-24T07:07:40+02:00",
+          "tree_id": "62c68a4e49b6fe598772ea1f5ff3d4e4efa98182",
+          "url": "https://github.com/developmentseed/titiler/commit/dd0e0d11147ffd0e10e089f8adb5de57c23075fa"
+        },
+        "date": 1761282604093,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.06,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 4.18,
             "unit": "s"
           },
           {
