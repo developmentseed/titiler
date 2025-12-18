470e0574df0fc7bb11dd7dc9991a9b4768fe7f64
vincentsarago <vincent.sarago@gmail.com>
2025-12-18 23:58:29 +0100

    fix aws lambda handlers

--- deployment/aws/lambda/handler.py
+++ deployment/aws/lambda/handler.py
@@ -1,11 +1,17 @@
 """AWS Lambda handler."""
 
+import asyncio
 import logging
 
 from mangum import Mangum
 
 from titiler.application.main import app
 from titiler.application.settings import ApiSettings
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
--- deployment/aws/lambda/xarray_handler.py
+++ deployment/aws/lambda/xarray_handler.py
@@ -1,10 +1,17 @@
 """AWS Lambda handler."""
 
+import asyncio
 import logging
 
 from mangum import Mangum
 
 from titiler.xarray.main import api_settings, app
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
