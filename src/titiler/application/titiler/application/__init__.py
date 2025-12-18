d488e60203f026590f0ad2e3fc4910aec8c62b5f
vincentsarago <vincent.sarago@gmail.com>
2025-12-18 23:20:13 +0100

    1.0.0 -> 1.0.1

--- src/titiler/application/titiler/application/__init__.py
+++ src/titiler/application/titiler/application/__init__.py
@@ -1,3 +1,3 @@
 """titiler.application"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
--- src/titiler/mosaic/titiler/mosaic/__init__.py
+++ src/titiler/mosaic/titiler/mosaic/__init__.py
@@ -1,6 +1,6 @@
 """titiler.mosaic"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from . import errors, factory  # noqa
 from .factory import MosaicTilerFactory  # noqa
