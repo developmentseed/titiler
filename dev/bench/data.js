468c18a964f0475444ab9c71959c1f26b947382c
github-action-benchmark <github@users.noreply.github.com>
2026-04-27 09:48:42 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for 17c17874aa6da70d2492d11c11791cc3e2e05475

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777282961553,
+  "lastUpdate": 1777283322420,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -22207,6 +22207,65 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/developmentseed/titiler/commit/214cedce1304003674009919dc606482e7547c8f"
         },
         "date": 1777282960157,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.02,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.05,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 3.07,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.34,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.04,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "vincentsarago",
+            "username": "vincentsarago"
+          },
+          "distinct": true,
+          "id": "17c17874aa6da70d2492d11c11791cc3e2e05475",
+          "message": "chore: add dependabot config",
+          "timestamp": "2026-04-27T11:46:50+02:00",
+          "tree_id": "440b01583441487a5da658e70dd9caa978e86033",
+          "url": "https://github.com/developmentseed/titiler/commit/17c17874aa6da70d2492d11c11791cc3e2e05475"
+        },
+        "date": 1777283321073,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
