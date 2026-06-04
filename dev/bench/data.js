b1a3d7a595885741707c046a7f455605c952ceb2
github-action-benchmark <github@users.noreply.github.com>
2026-06-04 15:03:12 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for 3c87ba8c91e1bc2ab2f227effa5f72e9776c9244

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780563771919,
+  "lastUpdate": 1780585392246,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -23702,6 +23702,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad elapsed_time",
             "value": 3.14,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.34,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.04,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "Vincent Sarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3c87ba8c91e1bc2ab2f227effa5f72e9776c9244",
+          "message": "feat: add min/max zoom query-parameters to tileset endpoint (#1416)",
+          "timestamp": "2026-06-04T17:01:09+02:00",
+          "tree_id": "a22bb7eb2beb117bfab0fecd6783f069c52ec1a5",
+          "url": "https://github.com/developmentseed/titiler/commit/3c87ba8c91e1bc2ab2f227effa5f72e9776c9244"
+        },
+        "date": 1780585391090,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.02,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.05,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 3.05,
             "unit": "s"
           },
           {
