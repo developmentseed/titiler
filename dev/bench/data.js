6a43215eded8aec6737841b7744f856bfc75278f
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 18:16:22 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for f5c307fd572fb90e905cc42f0d84726125087535

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784562982281,
+  "lastUpdate": 1784571382499,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -25000,6 +25000,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad elapsed_time",
             "value": 3.21,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.34,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.04,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent.sarago@gmail.com",
+            "name": "Vincent Sarago",
+            "username": "vincentsarago"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5c307fd572fb90e905cc42f0d84726125087535",
+          "message": "feat: extend tilejson and add get-renders method to tilerFactories (#1425)\n\n* feat: extend tilejson and add get-renders method to tilerFactories\n\n* fix: type\n\n* fix: type\n\n* fix: avoid breaking changes",
+          "timestamp": "2026-07-20T14:14:38-04:00",
+          "tree_id": "3a04b55181318cc9fbaf8fe3dc3d98d28b9ecabd",
+          "url": "https://github.com/developmentseed/titiler/commit/f5c307fd572fb90e905cc42f0d84726125087535"
+        },
+        "date": 1784571381691,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.02,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.2,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 3.1,
             "unit": "s"
           },
           {
