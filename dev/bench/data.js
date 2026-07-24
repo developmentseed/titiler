4264c9604390d106ae4e3a546ae64324d1c6a219
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 13:28:26 +0000

    add TiTiler performance Benchmarks (customSmallerIsBetter) benchmark result for a16802cdeaadcf803f73f5b649762f6806e57e0e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784817472599,
+  "lastUpdate": 1784899706921,
   "repoUrl": "https://github.com/developmentseed/titiler",
   "entries": {
     "TiTiler performance Benchmarks": [
@@ -25428,6 +25428,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "WGS1984Quad longest_transaction",
             "value": 0.04,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "guillaume@lostis.com",
+            "name": "Guillaume Lostis",
+            "username": "glostis"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a16802cdeaadcf803f73f5b649762f6806e57e0e",
+          "message": "fix: use environment_dependency in cogeo extension (#1462)",
+          "timestamp": "2026-07-24T09:26:20-04:00",
+          "tree_id": "343a80e86e3d09bc4b96d4b98ee345bf32b58fb8",
+          "url": "https://github.com/developmentseed/titiler/commit/a16802cdeaadcf803f73f5b649762f6806e57e0e"
+        },
+        "date": 1784899705629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "WebMercator data_transferred",
+            "value": 5.29,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WebMercator response_time",
+            "value": 0.02,
+            "unit": "s"
+          },
+          {
+            "name": "WebMercator longest_transaction",
+            "value": 0.15,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad elapsed_time",
+            "value": 2.69,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad data_transferred",
+            "value": 5.34,
+            "unit": "Megabytes"
+          },
+          {
+            "name": "WGS1984Quad response_time",
+            "value": 0.03,
+            "unit": "s"
+          },
+          {
+            "name": "WGS1984Quad longest_transaction",
+            "value": 0.03,
             "unit": "s"
           }
         ]
